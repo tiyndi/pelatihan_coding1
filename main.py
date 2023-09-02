015b66de45454c7e041bdc7dcfe3755a9951c80a
tiyandi <143859384+tiyndi@users.noreply.github.com>
2023-09-02 10:42:41 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# apel = 52.2
+# print(apel)
+# print(type(apel))
+# print(521)
+# nama_apel = "malang"
+# print(nama_apel)
+# print(type(nama_apel))
+
+# apel_benar = True
+# apel_salah = False
+# print(type(apel_benar))
+# print(apel_salah)
+
+# list_warna_apel = ['hijau','merah','kuning']
+# list_apel1 = ['manis', 2 , 3.0 ]
+# print(list_warna_apel)
+# print(list_apel1)
+
+# tuple_jumlah = (2, 5.25, "satu")
+# print(tuple_jumlah)
+
+# apel_dict = {'nama':'apel'}
+# print(apel_dict['nama'])
+
